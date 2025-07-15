6bcfd1e01454f9985b7da4a294f692f0a50b8a92
Luis Angel Garcia Louis <ludevgarcialouis@gmail.com>
2025-07-15 13:46:29 -0400

    updating youtube url

--- src/data/location-data.ts
+++ src/data/location-data.ts
@@ -7,5 +7,5 @@ export const LOCATION_DATA: LocationData = {
   googleMapsUrl:
     "https://www.google.com/maps/place/18%C2%B024'01.6%22N+69%C2%B005'09.3%22W/@18.4004421,-69.0884895,17z/data=!3m1!4b1!4m4!3m3!8m2!3d18.4004421!4d-69.0859146?hl=en&entry=ttu&g_ep=EgoyMDI1MDcwOS4wIKXMDSoASAFQAw%3D%3D",
   youtubeUrl:
-    "https://www.youtube.com/results?search_query=como+llegar+a+la+romana+republica+dominicana",
+    "https://www.youtube.com/watch?v=vhUqGtngdBU",
 };
